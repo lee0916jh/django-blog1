00edb7c5db2779b56793dfdec5d0fb666ed7e0c1
jlee <lee0916jh@gmail.com>
2021-02-16 21:07:49 -0600

    added Post and Comment model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -13,5 +14,29 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
